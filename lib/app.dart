d0ad47d2070313e589ac2bc8f0e9452221ec57bd
icodelifee <mithunsmenon@gmail.com>
2021-05-25 13:35:11 +0530

    feat: wrap material app with responsive sizer

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:moviewebsite/ui/homepage/homepage.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class App extends StatelessWidget {
   // root widget of the app
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      defaultTransition: Transition.cupertino,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        scaffoldBackgroundColor: Color(0xFF020916),
-        textTheme: GoogleFonts.interTextTheme(),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Color(0xFF182131),
+    return ResponsiveSizer(builder: (context, orientation, screenType) {
+      return GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        defaultTransition: Transition.cupertino,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          scaffoldBackgroundColor: Color(0xFF020916),
+          textTheme: GoogleFonts.interTextTheme(),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Color(0xFF182131),
+          ),
         ),
-      ),
-      home: Container(),
-    );
+        home: HomePage(),
+      );
+    });
   }
 }
