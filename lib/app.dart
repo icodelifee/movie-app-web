550422d162e9baa7a90c26700acedf5bb3d11f7c
icodelifee <mithunsmenon@gmail.com>
2021-05-25 13:33:30 +0530

    feat: add app theme

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class App extends StatelessWidget {
   // root widget of the app
@@ -8,6 +9,14 @@ class App extends StatelessWidget {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       defaultTransition: Transition.cupertino,
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Color(0xFF020916),
+        textTheme: GoogleFonts.interTextTheme(),
+        appBarTheme: AppBarTheme(
+          backgroundColor: Color(0xFF182131),
+        ),
+      ),
       home: Container(),
     );
   }
