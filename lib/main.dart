a67e99590cda7047af605c3f8c30f4c64811cd16
icodelifee <mithunsmenon@gmail.com>
2021-05-25 13:05:38 +0530

    feat(main): load the configuration during app intialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:global_configuration/global_configuration.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:moviewebsite/app.dart';
+import 'package:moviewebsite/config/example.config.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  GlobalConfiguration().loadFromMap(appConfig);
   runApp(ProviderScope(child: App()));
 }
