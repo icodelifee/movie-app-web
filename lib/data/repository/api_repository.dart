19a39043a4e2d5a3344c16dedc1bf9ee4919a84d
icodelifee <mithunsmenon@gmail.com>
2021-05-25 17:11:10 +0530

    feat: implement api repository and add methods to fetch movies

---
+++ lib/data/repository/api_repository.dart
@@ -0,0 +1,18 @@
+import 'package:dio/dio.dart';
+import 'package:moviewebsite/constants.dart';
+import 'package:moviewebsite/data/models/tmdb_response.dart';
+import 'package:retrofit/http.dart';
+
+part 'api_repository.g.dart';
+
+@RestApi(baseUrl: Constants.apiUrl)
+abstract class APIRepository {
+  factory APIRepository(
+    Dio dio,
+  ) = _APIRepository;
+  @GET('trending/all/day')
+  Future<TMDBResponse> getTrending(@Query('api_key') String apiKey);
+
+  @GET('movie/popular')
+  Future<TMDBResponse> getPopular(@Query('api_key') String apiKey);
+}
