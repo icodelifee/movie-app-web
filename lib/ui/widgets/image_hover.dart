d02e1ecd201e2fb73c232c0347e2b7bc4675f283
icodelifee <mithunsmenon@gmail.com>
2021-05-25 17:12:18 +0530

    feat: implement hoverwidget using hooks

---
+++ lib/ui/widgets/image_hover.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+class ImageHover extends HookWidget {
+  ImageHover({required this.child});
+  final Widget child;
+  final Offset translate = Offset(0, 0);
+  @override
+  Widget build(context) {
+    final elevation = useState(4.0);
+    final scale = useState(1.0);
+    return InkWell(
+      onTap: () {},
+      onHover: (value) {
+        if (value) {
+          elevation.value = 20.0;
+          scale.value = 1.07;
+        } else {
+          elevation.value = 4.0;
+          scale.value = 1.0;
+        }
+      },
+      child: Transform.translate(
+        offset: translate,
+        child: Transform.scale(
+          scale: scale.value,
+          child: Material(
+            color: Colors.transparent,
+            elevation: elevation.value,
+            child: child,
+          ),
+        ),
+      ),
+    );
+  }
+}
