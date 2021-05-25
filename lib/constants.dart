6fbd4600cc1bbb3a409b3b5298711bf1b1cccb04
icodelifee <mithunsmenon@gmail.com>
2021-05-25 17:07:31 +0530

    feat: add image base url to constants

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,7 @@
+import 'package:global_configuration/global_configuration.dart';
+
 class Constants {
   Constants._();
   static const apiUrl = 'https://api.themoviedb.org/3/';
+  static const imageUrl = 'https://image.tmdb.org/t/p/original';
 }
