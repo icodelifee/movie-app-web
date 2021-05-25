26fbb7755510dd90971e4c7120b772b997833dc1
icodelifee <mithunsmenon@gmail.com>
2021-05-25 12:17:42 +0530

    refractor: main.dart

---
+++ lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/get_navigation.dart';
+
+class App extends StatelessWidget {
+  // root widget of the app
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp();
+  }
+}
